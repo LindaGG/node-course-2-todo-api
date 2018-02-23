09fbbe7db6b75763436c2b227794cea0a99d2dde
Linda Gong <mengyugong5@gmail.com>
2018-02-23 16:10:27 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    console.log('Unable to connect to MongoDB Server');
+
+  }
+  console.log('Connect to MongoDB Server');
+
+  /*db.collection('Todos').find({
+    _id: new ObjectID('5a8f99221876a40d44910b8c')
+  }).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });*/
+
+  /*db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });*/
+
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch Users', err);
+  });
+
+
+  //db.close();
+});
